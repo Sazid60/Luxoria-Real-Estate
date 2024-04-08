5f0fc81a05d17e75a8749e6b531de9230f05e207
Sazid60 <shahnawazsazid60@gmail.com>
2024-04-08 10:22:13 +0600

    Footer Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
