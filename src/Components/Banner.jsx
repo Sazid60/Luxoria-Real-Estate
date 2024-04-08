50db5664907d0ebff693987a2cc6e1a7a506006b
Sazid60 <shahnawazsazid60@gmail.com>
2024-04-08 14:06:03 +0600

    Banner Section React Type Animation Addded

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,13 +7,21 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
+import { TypeAnimation } from 'react-type-animation';
+
 import { Autoplay } from 'swiper/modules';
 
 const Banner = () => {
     return (
         <div className='relative'>
-            <div className='absolute inset-0 flex flex-col items-center justify-center z-10'>
-                <h1 className='font-bold lg:text-6xl text-white'>Discover Your <br /> New <span className='text-[#E1B453]'>Home</span></h1>
+            <div className='absolute inset-0 flex flex-col z-10 translate-x-4 translate-y-8 md:translate-y-16 lg:translate-y-16'>
+                <h1 className='font-bold text-3xl md:text-4xl lg:text-6xl text-white mb-6'>Discover Your <br /> New <span className='text-[#E1B453]'>Home</span></h1>
+                <TypeAnimation className='text-white text-xl md:text-2xl lg:text-4xl font-semibold'
+                    sequence={['Single-Family', 600, ' Town-Houses', 600, 'Apartments', 600,'Student-Housing',600, 'Senior -Living-Communities',600, 'Vacation-Rentals',600]}
+                    // style={{ fontSize: '2em', fontWeight:700, color:"white" }}
+                    repeat={Infinity}
+                />
+
             </div>
 
             <div>
@@ -28,19 +36,19 @@ const Banner = () => {
                     className="mySwiper"
                 >
                     <SwiperSlide><div className='bg-[url(https://i.ibb.co/yVs9M4v/banner-1.jpg)] h-[200px] md:h-[300px] lg:h-[400px] w-full bg-cover bg-center bg-no-repeat'>
-                        <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-40'></div>
+                        <div className='absolute inset-0 bg-gradient-to-b from-black  to-transparent opacity-60'></div>
                     </div>
                     </SwiperSlide>
                     <SwiperSlide><div className='bg-[url(https://i.ibb.co/f91pGLL/banner-2.jpg)] h-[200px] md:h-[300px] lg:h-[400px] w-full bg-cover bg-center bg-no-repeat'>
-                        <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-40'></div>
+                        <div className='absolute inset-0 bg-gradient-to-b from-black  to-transparent opacity-60'></div>
                     </div>
                     </SwiperSlide>
                     <SwiperSlide><div className='bg-[url(https://i.ibb.co/Yy9xKtv/banner-3.jpg)] h-[200px] md:h-[300px] lg:h-[400px] w-full bg-cover bg-center bg-no-repeat'>
-                        <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-40'></div>
+                        <div className='absolute inset-0 bg-gradient-to-b from-black  to-transparent opacity-60'></div>
                     </div>
                     </SwiperSlide>
                     <SwiperSlide><div className='bg-[url(https://i.ibb.co/7SQXQBb/banner-4.jpg)] h-[200px] md:h-[300px] lg:h-[400px] w-full bg-cover bg-center bg-no-repeat'>
-                        <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-40'></div>
+                        <div className='absolute inset-0 bg-gradient-to-b from-black  to-transparent opacity-60'></div>
                     </div></SwiperSlide>
                 </Swiper>
             </div>
