4d0a435d8f2df215ff3aa62fe61ae0b6eb06f090
Sazid60 <shahnawazsazid60@gmail.com>
2024-04-09 12:14:25 +0600

    Card Updated

--- src/Components/EstateCards/EstateCards.jsx
+++ src/Components/EstateCards/EstateCards.jsx
@@ -6,26 +6,33 @@ import { TbRulerMeasure } from "react-icons/tb";
 const EstateCards = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
-            <div className="card card-compact bg-base-100 shadow-xl p-4 rounded-none">
-                <figure className=""><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+            <div className="bg-base-100 shadow-md p-2 rounded-none max-w-xs">
+                <figure className="w-full"><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="" />
                 </figure>
-                <div className="p-4">
+                <div className="p-2">
                     <div>
-                        <h2 className="font-bold text-lg md:text-lg lg:text-xl mb-6">Single Family Homes</h2>
-                        <div className="flex items-center justify-between">
-                            <p className="font-bold text-sm md:text-lg lg:text-lg text-[#E1B453] flex items-center gap-2 "><span className="text-black"><TbRulerMeasure /></span>2500 SQFT</p>
-                            <h1 className="font-bold text-sm  bg-[#E1B453] flex justify-center items-center px-2 py-1 text-white rounded-sm">Status</h1>
+                        <h2 className="font-bold text-lg mb-4 text-[#E1B453]">Single Family Homes</h2>
+                        <div className="flex items-center justify-between mb-4">
+                            <p className="font-bold text-sm text-[#E1B453] flex items-center gap-1 "><span className="text-black"><TbRulerMeasure /></span>2500 SQFT</p>
+                            <h1 className="font-bold text-xs bg-[#E1B453] flex justify-center items-center px-1 py-0.5 text-white rounded-sm">Status</h1>
                         </div>
                     </div>
-                    <div className="flex justify-between mb-4 mt-4 text-center">
-                        <p className="font-bold text-sm md:text-sm lg:text-lg flex items-center gap-2 "><span className="text-black"><CiLocationOn /></span> Dhanmondi</p>
-                        <p className="font-bold text-sm md:text-sm lg:text-lg flex items-center gap-2 "><span className="text-black"><CiBadgeDollar /></span>2500$/Month</p>
+                    <div className="flex justify-between mb-4 mt-2 text-center">
+                        <p className="font-bold text-xs flex items-center gap-1 "><span className="text-black"><CiLocationOn /></span> Dhanmondi</p>
+                        <p className="font-bold text-xs  flex items-center gap-1 "><span className="text-black"><CiBadgeDollar /></span>2500$/Month</p>
+                    </div>
+                    <div className="flex gap-2 mb-2 mt-2 text-center">
+                        <p className="border border-[#E1B453] p-1 rounded-sm text-xs ">Graden</p>
+                        <p className="border border-[#E1B453] p-1 rounded-sm text-xs ">2 Bed</p>
+                        <p className="border border-[#E1B453] p-1 rounded-sm text-xs ">Backyard</p>
                     </div>
                     <div className="card-actions w-full">
-                        <button className="btn bg-[#E1B453] w-full text-white">Buy Now</button>
+                        <button className="btn bg-[#E1B453] w-full text-white text-xs btn-sm mt-2">View Details</button>
                     </div>
                 </div>
             </div>
+
+
         </div>
     );
 };
