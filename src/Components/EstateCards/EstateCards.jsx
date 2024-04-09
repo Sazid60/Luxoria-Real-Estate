36e0b10a8222fdaae64070c771049a0c5f5e34f1
Sazid60 <shahnawazsazid60@gmail.com>
2024-04-09 12:53:26 +0600

    Json Data Created

--- src/Components/EstateCards/EstateCards.jsx
+++ src/Components/EstateCards/EstateCards.jsx
@@ -5,13 +5,16 @@ import { TbRulerMeasure } from "react-icons/tb";
 
 const EstateCards = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
-            <div className="bg-base-100 shadow-md p-2 rounded-none max-w-xs">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6 lg:px-28">
+            {/* Card */}
+            <div className="bg-base-100 shadow-md p-2 rounded-none ">
                 <figure className="w-full"><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="" />
                 </figure>
                 <div className="p-2">
                     <div>
-                        <h2 className="font-bold text-lg mb-4 text-[#E1B453]">Single Family Homes</h2>
+                        <h2 className="font-bold text-lg mb-2 text-[#E1B453]">Majestic Manor</h2>
+                        <h2 className="font-medium text-xs mb-4">Single Family Homes</h2>
+
                         <div className="flex items-center justify-between mb-4">
                             <p className="font-bold text-sm text-[#E1B453] flex items-center gap-1 "><span className="text-black"><TbRulerMeasure /></span>2500 SQFT</p>
                             <h1 className="font-bold text-xs bg-[#E1B453] flex justify-center items-center px-1 py-0.5 text-white rounded-sm">Status</h1>
@@ -31,8 +34,6 @@ const EstateCards = () => {
                     </div>
                 </div>
             </div>
-
-
         </div>
     );
 };
