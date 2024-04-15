a2e5db70ca09325a717714a6a52d9bd772e3b06d
Sazid60 <shahnawazsazid60@gmail.com>
2024-04-15 17:26:34 +0600

    Profile Update Issue Fixed

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import { useContext, useState } from "react";
 import toast from 'react-hot-toast';
@@ -13,7 +13,7 @@ const Profile = () => {
     const [name, setName] = useState(user.displayName);
     const [email, setEmail] = useState(user.email);
     const [photoUrl, setPhotoUrl] = useState(user.photoURL);
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
     // Function to handle form submission
     const handleSubmit = (event) => {
@@ -24,11 +24,10 @@ const Profile = () => {
             .then(() => {
 
                 // Since the profile is not updating instantly
-                setUser({ displayName: name, photoURL: photoUrl })
+                setUser({ displayName: name, photoURL: photoUrl,email:email })
 
                 toast.success('Update Successful');
                 setSuccess("Successfully Update")
-                navigate("/")
             })
             .catch(error => {
                 setErrorMessage(error.message)
@@ -43,8 +42,8 @@ const Profile = () => {
             </Helmet>
             <div className="flex flex-col justify-center items-center min-h-[40vh]">
                 <div className="flex justify-center items-center lg:justify-normal flex-col lg:w-[50%]">
-                    <img src={user.photoURL} className="h-24 w-24 lg:h-24 lg:w-24 rounded-full" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="320"/>
-                    <h1 className="text-2xl font-bold mt-4"  data-aos="fade-up" data-aos-duration="500" data-aos-delay="400"> {user.displayName.toUpperCase()}</h1>
+                    <img src={user.photoURL} className="h-24 w-24 lg:h-24 lg:w-24 rounded-full" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="320" />
+                    <h1 className="text-2xl font-bold mt-4" data-aos="fade-up" data-aos-duration="500" data-aos-delay="400"> {user.displayName.toUpperCase()}</h1>
                 </div>
                 <div className="w-full md:w-[70%]lg:w-[70%] bg-white">
                     <h1 className="text-sm font-bold mt-6 ml-3" data-aos="fade-up" data-aos-duration="500" data-aos-delay="420">Want To Update Profile ?</h1>
@@ -67,8 +66,9 @@ const Profile = () => {
                             </label>
                             <input type="text" placeholder="Photo URL" name="photoUrl" value={photoUrl} onChange={(e) => setPhotoUrl(e.target.value)} className="input input-bordered text-gray-700 bg-transparent border-gray-300" required />
                         </div>
-                        <div className="form-control mt-2 flex justify-center items-center" >
+                        <div className="form-control mt-3 flex flex-row justify-center items-center gap-2" >
                             <button type="submit" className="btn w-36 btn-md bg-[#E1B453] border-0 text-white mb-6">Update</button>
+                            <Link to={"/"}> <button className="btn w-36 btn-md bg-[#f0d08c] text-black border-0 mb-6">Explore</button></Link>
                         </div>
                     </form>
                 </div>
